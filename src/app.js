5a52ff851d9a7bf6edeb900f744916a6afbebdaa
LexiphanicStroon <nate_reeves@outlook.com>
2024-07-13 13:23:57 -0500

    update for temp page

--- src/app.js
+++ src/app.js
@@ -2,13 +2,3 @@
 particlesJS.load('particles-js', 'particles.json', function () {
   console.log('callback - particles.js config loaded');
 });
-
-document
-  .querySelector('#scroll-btn')
-  .addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector('#services').scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  });
